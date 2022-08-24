2ed1e5734f4fbd218a2ffb4dee90a8f82561bc4c
prakash817 <96722484+prakash817@users.noreply.github.com>
2022-08-24 10:19:41 +0530

    nvbar completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import Home from './pages/Home';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Navbar />
+      {/* <Home></Home> */}
     </div>
   );
 }
