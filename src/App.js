bd96504392d4545f4b36a3843ff1aeeec08342d5
Shubham Barore <shubham07barore@gmail.com>
2022-08-24 09:41:37 +0530

    day2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Home from './pages/Home';
+import { Routes, Route } from "react-router-dom";
+import { Box } from "@chakra-ui/react"
+import HowItWorks from './pages/HowItWorks';
+import UseCases from './pages/UseCases';
 
 function App() {
   return (
-    <div className="App">
-      <Home></Home>
-    </div>
+    <Box className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/howitworks" element={<HowItWorks />} />
+        <Route path="/usecases" element={<UseCases />} />
+      </Routes>
+    </Box>
   );
 }
 
