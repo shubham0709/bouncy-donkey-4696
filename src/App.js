caab865cb96dd161f81700a17267bc5e0068e605
Shubham Barore <shubham07barore@gmail.com>
2022-08-24 21:48:14 +0530

    landing page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import { Routes, Route } from "react-router-dom";
 import { Box } from "@chakra-ui/react"
 import HowItWorks from './pages/HowItWorks';
 import UseCases from './pages/UseCases';
+import Project from './pages/project/Project';
+import AddProject from './pages/project/AddProject';
 
 function App() {
   return (
-    <Box className="App">
+    <Box align="center">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/howitworks" element={<HowItWorks />} />
         <Route path="/usecases" element={<UseCases />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/newproject" element={<AddProject />} />
       </Routes>
     </Box>
   );
