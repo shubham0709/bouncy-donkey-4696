667440f44a9978430f7ee1b98d8aae9826b7b9c9
Shubham Barore <shubham07barore@gmail.com>
2022-08-28 10:19:33 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./footer.css";
 const Footer = () => {
+  const navigate = useNavigate();
   return (
     <div className="footer">
       <div className="footerDiv">
