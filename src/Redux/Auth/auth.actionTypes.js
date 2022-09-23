73ddd544722eea627edf8322d89ada95e8ff1feb
sumeet.rana <sumeet.rana@innovaccer.com>
2022-09-23 11:47:58 +0530

    feat/deleted files to review

--- src/Redux/Auth/auth.actionTypes.js
+++
@@ -1,7 +0,0 @@
-export const USER_REGISTER = "user/register";
-
-export const USER_LOGIN_REQUEST = "user/login/request";
-export const USER_LOGIN_SUCCESS = "user/login/success";
-export const USER_LOGIN_FAILED = "user/login/failed";
-
-export const USER_LOGOUT = "user/logout";
